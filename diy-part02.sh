84bfc4e529c409132266840bade31f8f88627cd6
0118Add <60157610+0118Add@users.noreply.github.com>
2022-10-22 22:42:28 +0800

    Update diy-part02.sh

--- diy-part02.sh
+++ diy-part02.sh
@@ -74,8 +74,8 @@ svn export -q https://github.com/immortalwrt/luci/branches/openwrt-21.02/libs/lu
 ln -sf ../../../feeds/luci/libs/luci-lib-fs ./package/feeds/luci/luci-lib-fs
 
 # AutoCore
-svn export -q https://github.com/immortalwrt/immortalwrt/branches/openwrt-21.02/package/emortal/autocore package/new/autocore
-#svn export -q https://github.com/jinlife/OpenWrt-Autobuild/trunk/autocore package/new/autocore
+svn export -q https://github.com/immortalwrt/immortalwrt/branches/master/package/emortal/autocore package/new/autocore
+svn export -q https://github.com/immortalwrt/packages/branches/master/utils/mhz package/utils/mhz
 
 # default settings and translation
 svn export -q https://github.com/immortalwrt/immortalwrt/branches/openwrt-21.02/package/emortal/default-settings package/new/default-settings
