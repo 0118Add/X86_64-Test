b75113fb369ccf5fe7ed1f25004b24ca7c8b2076
0118Add <60157610+0118Add@users.noreply.github.com>
2022-11-20 10:03:03 +0800

    Update diy-part02.sh

--- diy-part02.sh
+++ diy-part02.sh
@@ -9,8 +9,8 @@
 # File name: diy-part02.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 
-#Default IP
-sed -i 's#192.168.1.1#192.168.2.1#g' package/base-files/files/bin/config_generate
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # alist
 rm -rf feeds/packages/lang/golang
