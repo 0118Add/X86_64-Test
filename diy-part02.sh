87f94a9cd8351833d2f157db9516ad5bc8e5975a
0118Add <60157610+0118Add@users.noreply.github.com>
2023-07-18 14:35:51 +0800

    Update diy-part02.sh

--- diy-part02.sh
+++ diy-part02.sh
@@ -13,7 +13,7 @@
 rm -rf package/feeds/luci/luci-app-apinger
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
 # 更改固件版本信息
 sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION=''|g" package/base-files/files/etc/openwrt_release
