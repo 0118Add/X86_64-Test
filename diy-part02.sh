0e28a0eb2586e892bb092017f8ca5ed1bb55e143
0118Add <60157610+0118Add@users.noreply.github.com>
2023-12-03 16:01:26 +0800

    Update diy-part02.sh

--- diy-part02.sh
+++ diy-part02.sh
@@ -161,9 +161,9 @@ sed -i 's#net.netfilter.nf_conntrack_max=16384#net.netfilter.nf_conntrack_max=65
 # R8168驱动
 git clone -b master --depth 1 https://github.com/BROBIRD/openwrt-r8168.git package/new/r8168
 # R8152驱动
-svn export -q https://github.com/0118Add/openwrt-packages/trunk/r8152 ./package/new/r8152
+svn export -q https://github.com/0118Add/openwrt-packages/trunk/r8152 package/new/r8152
 # r8125驱动
-svn export -q https://github.com/0118Add/openwrt-packages/trunk/r8125 ./package/new/r8152
+svn export -q https://github.com/0118Add/openwrt-packages/trunk/r8125 package/new/r8125
 
 #sed -i 's/ShadowSocksR Plus+/SSR Plus+/g' package/new/luci-app-ssr-plus/luasrc/controller/shadowsocksr.lua
 #sed -i 's/Frp 内网穿透/内网穿透/g' package/new/luci-app-frpc/po/zh-cn/frp.po
