96f1adb9edf640fc7bb01e6dc580dddb883226fc
0118Add <60157610+0118Add@users.noreply.github.com>
2023-12-03 13:06:19 +0800

    Update diy-part02.sh

--- diy-part02.sh
+++ diy-part02.sh
@@ -158,10 +158,13 @@ ln -sf ../../../feeds/luci/applications/luci-app-wechatpush ./package/feeds/luci
 # net.netfilter.nf_conntrack_max from 16384 to 65535
 sed -i 's#net.netfilter.nf_conntrack_max=16384#net.netfilter.nf_conntrack_max=65535#g' package/kernel/linux/files/sysctl-nf-conntrack.conf
 
-# kernel
-#svn export -q https://github.com/immortalwrt/immortalwrt/branches/openwrt-23.05/package/kernel/r8125 package/new/r8125
-#svn export -q https://github.com/immortalwrt/immortalwrt/branches/openwrt-23.05/package/kernel/r8152 package/new/r8152
-#svn export -q https://github.com/immortalwrt/immortalwrt/branches/openwrt-23.05/package/kernel/r8168 package/new/r8168
+# R8168驱动
+git clone -b master --depth 1 https://github.com/BROBIRD/openwrt-r8168.git package/new/r8168
+patch -p1 <../PATCH/r8168/r8168-fix_LAN_led-for_r4s-from_TL.patch
+# R8152驱动
+cp -rf ../immortalwrt/package/kernel/r8152 ./package/new/r8152
+# r8125驱动
+git clone https://github.com/sbwml/package_kernel_r8125 package/new/r8125
 
 #sed -i 's/ShadowSocksR Plus+/SSR Plus+/g' package/new/luci-app-ssr-plus/luasrc/controller/shadowsocksr.lua
 #sed -i 's/Frp 内网穿透/内网穿透/g' package/new/luci-app-frpc/po/zh-cn/frp.po
