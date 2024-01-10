18345a6dddb20a6e9af0e365c4b1edb79d38e077
0118Add <60157610+0118Add@users.noreply.github.com>
2024-01-10 09:02:14 +0800

    Update diy-part.sh

--- diy-part.sh
+++ diy-part.sh
@@ -15,4 +15,25 @@
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git openwrt-packages https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default
+# Modify default IP
+sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+
+# node - prebuilt
+rm -rf feeds/packages/lang/node
+git clone https://github.com/sbwml/feeds_packages_lang_node-prebuilt feeds/packages/lang/node
+
+# 移除 SNAPSHOT 标签
+sed -i 's,-SNAPSHOT,,g' include/version.mk
+sed -i 's,-SNAPSHOT,,g' package/base-files/image-config.in
+
+git clone https://github.com/kiddin9/openwrt-packages package/openwrt-packages
+
+# net.netfilter.nf_conntrack_max from 16384 to 65535
+sed -i 's#net.netfilter.nf_conntrack_max=16384#net.netfilter.nf_conntrack_max=65535#g' package/kernel/linux/files/sysctl-nf-conntrack.conf
+
+# R8168驱动
+#git clone -b master --depth 1 https://github.com/BROBIRD/openwrt-r8168.git package/new/r8168
+# R8152驱动
+#svn export -q https://github.com/0118Add/openwrt-packages/trunk/r8152 package/new/r8152
+# r8125驱动
+#svn export -q https://github.com/0118Add/openwrt-packages/trunk/r8125 package/new/r8125
