90062c1d0ddb8f6cc83ab72ddec091a75f9eaff4
0118Add <60157610+0118Add@users.noreply.github.com>
2024-01-10 12:30:27 +0800

    Update diy-part.sh

--- diy-part.sh
+++ diy-part.sh
@@ -26,7 +26,8 @@ git clone https://github.com/sbwml/feeds_packages_lang_node-prebuilt feeds/packa
 sed -i 's,-SNAPSHOT,,g' include/version.mk
 sed -i 's,-SNAPSHOT,,g' package/base-files/image-config.in
 
-git clone https://github.com/kiddin9/openwrt-packages package/openwrt-packages
+#git clone https://github.com/kiddin9/openwrt-packages package/openwrt-packages
+#rm -rf package/openwrt-packages/{automount,v2ray-core,v2ray-geodata,sing-box}
 
 # net.netfilter.nf_conntrack_max from 16384 to 65535
 sed -i 's#net.netfilter.nf_conntrack_max=16384#net.netfilter.nf_conntrack_max=65535#g' package/kernel/linux/files/sysctl-nf-conntrack.conf
