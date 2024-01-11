9ea0ae5bc0c88f8a3f3e928e9a1c1e2ba5df3c65
0118Add <60157610+0118Add@users.noreply.github.com>
2024-01-11 13:18:02 +0800

    Update diy-part.sh

--- diy-part.sh
+++ diy-part.sh
@@ -19,14 +19,7 @@ sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 rm -rf feeds/packages/lang/node
 git clone https://github.com/sbwml/feeds_packages_lang_node-prebuilt feeds/packages/lang/node
 
-rm -rf feeds/packages/utils/coremark
-#rm -rf feeds/luci/modules/luci-base
-#rm -rf feeds/luci/modules/luci-mod-status
-rm -rf feeds/luci/applications/luci-app-ttyd
-rm -rf feeds/luci/applications/luci-app-zerotier
-#git clone https://github.com/0118Add/openwrt-package package/openwrt-packages
-
-# 移除 SNAPSHOT 标签
+ # 移除 SNAPSHOT 标签
 sed -i 's,-SNAPSHOT,,g' include/version.mk
 sed -i 's,-SNAPSHOT,,g' package/base-files/image-config.in
 
@@ -34,32 +27,32 @@ sed -i 's,-SNAPSHOT,,g' package/base-files/image-config.in
 #git clone https://github.com/sbwml/luci-app-alist package/alist
 
 # SSRP & Passwall
-git clone https://github.com/sbwml/openwrt_helloworld package/helloworld -b v5
+#git clone https://github.com/sbwml/openwrt_helloworld package/helloworld -b v5
 #git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall-packages
 
 # OpenClash
-git clone -b dev --depth 1 https://github.com/vernesong/OpenClash package/luci-app-openclash
+#git clone -b dev --depth 1 https://github.com/vernesong/OpenClash package/luci-app-openclash
 
 # Daed
 git clone https://github.com/sbwml/luci-app-daed-next package/new/luci-app-daed-next
 
 # homeproxy
-git clone -b dev --depth 1 https://github.com/immortalwrt/homeproxy package/homeproxy
+#git clone -b dev --depth 1 https://github.com/immortalwrt/homeproxy package/homeproxy
 
 # unblockneteasemusic
-git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/luci-app-unblockneteasemusic
-sed -i 's/解除网易云音乐播放限制/音乐解锁/g' package/luci-app-unblockneteasemusic/root/usr/share/luci/menu.d/luci-app-unblockneteasemusic.json
+#git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/luci-app-unblockneteasemusic
+#sed -i 's/解除网易云音乐播放限制/音乐解锁/g' package/luci-app-unblockneteasemusic/root/usr/share/luci/menu.d/luci-app-unblockneteasemusic.json
 
 # wechatpush
-git clone https://github.com/tty228/luci-app-wechatpush package/luci-app-wechatpush
+#git clone https://github.com/tty228/luci-app-wechatpush package/luci-app-wechatpush
 
 # turboacc
-git clone https://github.com/chenmozhijin/turboacc package/new/luci-app-turboacc
-curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
-sed -i 's/Turbo ACC 网络加速/网络加速/g' package/turboacc/luci-app-turboacc/po/zh-cn/turboacc.po
+#git clone https://github.com/chenmozhijin/turboacc package/new/luci-app-turboacc
+#curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
+#sed -i 's/Turbo ACC 网络加速/网络加速/g' package/turboacc/luci-app-turboacc/po/zh-cn/turboacc.po
 
 # net.netfilter.nf_conntrack_max from 16384 to 65535
 sed -i 's#net.netfilter.nf_conntrack_max=16384#net.netfilter.nf_conntrack_max=65535#g' package/kernel/linux/files/sysctl-nf-conntrack.conf
 
 # R8168驱动
-git clone -b master --depth 1 https://github.com/BROBIRD/openwrt-r8168.git package/new/r8168
+#git clone -b master --depth 1 https://github.com/BROBIRD/openwrt-r8168.git package/new/r8168
