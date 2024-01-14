1496f0a1b661b8a53b98f98a4cf3c64367e27f32
0118Add <60157610+0118Add@users.noreply.github.com>
2024-01-14 13:36:03 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,19 @@
 
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#git_sparse_clone() {
+#    branch="$1" rurl="$2" repodir="$3"
+#    git clone -b $branch --depth=1 --filter=blob:none --sparse $rurl package/cache
+#    git -C package/cache sparse-checkout set $repodir
+#    mv -f package/cache/$repodir package
+#    rm -rf package/cache
+#}
+
+#git_sparse_clone main https://github.com/linkease/nas-packages-luci.git luci/luci-app-ddnsto
+#git_sparse_clone master https://github.com/linkease/nas-packages.git network/services/ddnsto
+#git_sparse_clone main https://github.com/ophub/luci-app-amlogic.git luci-app-amlogic
+#git_sparse_clone master https://github.com/kiddin9/openwrt-packages.git luci-app-onliner
+#git_sparse_clone master https://github.com/kiddin9/openwrt-packages.git luci-app-turboacc
+#git_sparse_clone master https://github.com/kiddin9/openwrt-packages.git luci-app-control-timewol
+#git_sparse_clone master https://github.com/lisaac/luci-app-dockerman.git applications/luci-app-dockerman
+#git_sparse_clone master https://github.com/vernesong/OpenClash.git luci-app-openclash
