b26729325def7d9ecd2ef839e437c8382ceed80d
0118Add <60157610+0118Add@users.noreply.github.com>
2024-01-17 12:55:59 +0800

    Update diy-part02.sh

--- diy-part02.sh
+++ diy-part02.sh
@@ -143,18 +143,19 @@ merge_package https://github.com/immortalwrt/luci luci/libs/luci-lib-fs
 
 
 # AutoCore
-#rm -rf feeds/packages/utils/coremark
+rm -rf feeds/packages/utils/coremark
 #cp -rf $GITHUB_WORKSPACE/general/coremark package/new/coremark
+curl -fsSL https://raw.githubusercontent.com/immortalwrt/packages/master/utils/coremark > ./feeds/packages/utils/coremark
 #merge_package https://github.com/immortalwrt/packages packages/utils/coremark
 #cp -rf $GITHUB_WORKSPACE/general/autocore package/new/autocore
-#merge_package https://github.com/immortalwrt/immortalwrt immortalwrt/package/emortal/autocore
+merge_package https://github.com/immortalwrt/immortalwrt immortalwrt/package/emortal/autocore
 #cp -rf $GITHUB_WORKSPACE/general/mhz package/new/mhz
 #merge_package https://github.com/immortalwrt/immortalwrt immortalwrt/package/utils/mhz
-#rm -rf feeds/luci/modules/luci-base
-#wget -P luci/modules https://github.com/immortalwrt/luci/raw/openwrt-23.05/modules/luci-base
+rm -rf feeds/luci/modules/luci-base
+curl -fsSL https://raw.githubusercontent.com/immortalwrt/luci/master/modules/luci-base > ./feeds/luci/modules/luci-base
 #cp -rf $GITHUB_WORKSPACE/general/luci-base feeds/luci/modules/luci-base
-#rm -rf feeds/luci/modules/luci-mod-status
-#wget -P luci/modules https://github.com/immortalwrt/luci/raw/openwrt-23.05/modules/luci-mod-status
+rm -rf feeds/luci/modules/luci-mod-status
+curl -fsSL https://raw.githubusercontent.com/immortalwrt/luci/master/modules/luci-mod-status > ./feeds/luci/modules/luci-mod-status
 #cp -rf $GITHUB_WORKSPACE/general/luci-mod-status feeds/luci/modules/luci-mod-status
 
 # default settings and translation
