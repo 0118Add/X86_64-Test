ff14b180b5486c85b66524c00ff2b924f7bd6627
0118Add <60157610+0118Add@users.noreply.github.com>
2024-01-23 20:06:12 +0800

    Update diy-part02.sh

--- diy-part02.sh
+++ diy-part02.sh
@@ -145,7 +145,7 @@ merge_package https://github.com/immortalwrt/luci luci/libs/luci-lib-fs
 rm -rf feeds/packages/utils/coremark
 #merge_package https://github.com/immortalwrt/packages packages/utils/coremark
 #merge_package https://github.com/immortalwrt/immortalwrt immortalwrt/package/emortal/autocore
-#merge_package https://github.com/immortalwrt/immortalwrt immortalwrt/package/utils/mhz
+merge_package https://github.com/immortalwrt/immortalwrt immortalwrt/package/utils/mhz
 rm -rf feeds/luci/modules/luci-base
 rm -rf feeds/luci/modules/luci-mod-status
 # 克隆immortalwrt-luci仓库
