2490609833f846203eb536612bf7b3bd92676863
0118Add <60157610+0118Add@users.noreply.github.com>
2024-03-25 18:16:08 +0800

    Update ARMv8.sh

--- ARMv8.sh
+++ ARMv8.sh
@@ -87,7 +87,7 @@ git clone https://github.com/8688Add/luci-app-dae package/luci-app-dae
 
 # Shared for PassWall and ShadowsocksR Plus+
 rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box}
-git clone https://github.com/sbwml/openwrt_helloworld package/new/helloworld -b v5
+git clone https://github.com/8688Add/openwrt_helloworld package/new/helloworld -b v5
 #merge_package https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/new/luci-app-ssr-plus
 #merge_package https://github.com/kiddin9/openwrt-packages openwrt-packages/luci-app-passwall
 #merge_package https://github.com/kiddin9/openwrt-packages openwrt-packages/luci-app-passwall2
