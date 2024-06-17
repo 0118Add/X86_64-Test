79c0fd93cddf7be7603fcf31d8a8afbd6a07f5ef
0118Add <60157610+0118Add@users.noreply.github.com>
2024-06-17 14:25:37 +0800

    Update 6.10.sh

--- custom/6.10.sh
+++ custom/6.10.sh
@@ -77,6 +77,8 @@ merge_package https://github.com/kiddin9/openwrt-packages openwrt-packages/lua-m
 
 # homeproxy
 git clone --depth=1 https://github.com/immortalwrt/homeproxy package/homeproxy
+sed -i "s/ImmortalWrt/OpenWrt/g" package/homeproxy/po/zh_Hans/homeproxy.po
+sed -i "s/ImmortalWrt proxy/OpenWrt proxy/g" package/homeproxy/htdocs/luci-static/resources/view/homeproxy/{client.js,server.js}
 
 # Release Ram
 merge_package https://github.com/kiddin9/openwrt-packages openwrt-packages/luci-app-ramfree
