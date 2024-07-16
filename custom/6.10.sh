f192b0e3ead0aeef2bc6777587ce2037c3601cbd
0118Add <60157610+0118Add@users.noreply.github.com>
2024-07-16 09:42:54 +0800

    Update 6.10.sh

--- custom/6.10.sh
+++ custom/6.10.sh
@@ -97,6 +97,10 @@ sed -i "s/ImmortalWrt proxy/OpenWrt proxy/g" package/homeproxy/htdocs/luci-stati
 # mihomo
 git clone https://github.com/morytyann/OpenWrt-mihomo  package/openwrt-mihomo
 
+# custom packages
+rm -rf feeds/packages/utils/coremark
+merge_package https://github.com/8688Add/openwrt_pkgs openwrt_pkgs/coremark
+
 # Release Ram
 merge_package https://github.com/kiddin9/openwrt-packages openwrt-packages/luci-app-ramfree
 
