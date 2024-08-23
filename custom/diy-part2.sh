ad2960e33d43e497133ccfa5f3e21b5c8c905dd4
0118Add <60157610+0118Add@users.noreply.github.com>
2024-08-23 14:27:21 +0800

    Update diy-part2.sh

--- custom/diy-part2.sh
+++ custom/diy-part2.sh
@@ -200,11 +200,11 @@ sed -i 's/Turbo ACC 网络加速/网络加速/g' package/turboacc/luci-app-turbo
 sed -i 's#net.netfilter.nf_conntrack_max=16384#net.netfilter.nf_conntrack_max=65535#g' package/kernel/linux/files/sysctl-nf-conntrack.conf
 
 # Realtek driver - R8168 & R8125 & R8126 & R8152 & R8101
-git clone https://github.com/sbwml/package_kernel_r8168 package/kernel/r8168
-git clone https://github.com/sbwml/package_kernel_r8152 package/kernel/r8152
-git clone https://github.com/sbwml/package_kernel_r8101 package/kernel/r8101
-git clone https://github.com/sbwml/package_kernel_r8125 package/kernel/r8125
-git clone https://github.com/sbwml/package_kernel_r8126 package/kernel/r8126
+#git clone https://github.com/sbwml/package_kernel_r8168 package/kernel/r8168
+#git clone https://github.com/sbwml/package_kernel_r8152 package/kernel/r8152
+#git clone https://github.com/sbwml/package_kernel_r8101 package/kernel/r8101
+#git clone https://github.com/sbwml/package_kernel_r8125 package/kernel/r8125
+#git clone https://github.com/sbwml/package_kernel_r8126 package/kernel/r8126
 
 # 替换文件
 #curl -fsSL https://raw.githubusercontent.com/0118Add/X86_64-Test/main/10_system.js > ./feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
