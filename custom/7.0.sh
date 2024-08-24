6d72a004c4fb29bd3fdb5f630db28e29b91fa6ce
0118Add <60157610+0118Add@users.noreply.github.com>
2024-08-24 16:36:29 +0800

    Update 7.0.sh

--- custom/7.0.sh
+++ custom/7.0.sh
@@ -101,21 +101,6 @@ sed -i "s/ImmortalWrt proxy/OpenWrt proxy/g" package/homeproxy/luci-app-homeprox
 
 # mihomo
 git clone https://github.com/morytyann/OpenWrt-mihomo  package/openwrt-mihomo
-mkdir -p files/etc/mihomo/run/ui
-curl -Lso files/etc/mihomo/run/Country.mmdb https://github.com/NobyDa/geoip/raw/release/Private-GeoIP-CN.mmdb
-curl -Lso files/etc/mihomo/run/GeoIP.dat https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/geoip-lite.dat
-curl -Lso files/etc/mihomo/run/GeoSite.dat https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/geosite.dat
-curl -Lso metacubexd-gh-pages.tar.gz https://github.com/MetaCubeX/metacubexd/archive/refs/heads/gh-pages.tar.gz
-tar zxf metacubexd-gh-pages.tar.gz
-mv metacubexd-gh-pages files/etc/mihomo/run/ui/metacubexd
-
-# sysupgrade keep files
-echo "/etc/hotplug.d/iface/*.sh" >> files/etc/sysupgrade.conf
-echo "/etc/mihomo/run/proxies" >> files/etc/sysupgrade.conf
-echo "/etc/mihomo/run/rules" >> files/etc/sysupgrade.conf
-echo "/etc/mihomo/run/config.yaml" >> files/etc/sysupgrade.conf
-echo "/opt" >> files/etc/sysupgrade.conf
-echo "/etc/init.d/nezha-service" >> files/etc/sysupgrade.conf
 
 # custom packages
 rm -rf feeds/packages/utils/coremark
