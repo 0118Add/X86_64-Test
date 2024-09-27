926931a9bf56dc78229f7dbce4c78b769a132d94
0118Add <60157610+0118Add@users.noreply.github.com>
2024-09-27 13:27:58 +0800

    Update 6.11.sh

--- custom/6.11.sh
+++ custom/6.11.sh
@@ -172,3 +172,58 @@ cp -rf $GITHUB_WORKSPACE/general/ruby feeds/packages/lang/ruby
 # comment out the following line to restore the full description
 #sed -i '/# timezone/i grep -q '\''/tmp/sysinfo/model'\'' /etc/rc.local || sudo sed -i '\''/exit 0/i [ "$(cat /sys\\/class\\/dmi\\/id\\/sys_vendor 2>\\/dev\\/null)" = "Default string" ] \&\& echo "x86_64" > \\/tmp\\/sysinfo\\/model'\'' /etc/rc.local\n' package/default-settings/default/zzz-default-settings
 sed -i '/# timezone/i sed -i "s/\\(DISTRIB_DESCRIPTION=\\).*/\\1'\''OpenWrt $(sed -n "s/DISTRIB_DESCRIPTION='\''OpenWrt \\([^ ]*\\) .*/\\1/p" /etc/openwrt_release)'\'',/" /etc/openwrt_release\nsource /etc/openwrt_release \&\& sed -i -e "s/distversion\\s=\\s\\".*\\"/distversion = \\"$DISTRIB_ID $DISTRIB_RELEASE ($DISTRIB_REVISION)\\"/g" -e '\''s/distname    = .*$/distname    = ""/g'\'' /usr/lib/lua/luci/version.lua\nsed -i "s/luciname    = \\".*\\"/luciname    = \\"LuCI openwrt-23.05\\"/g" /usr/lib/lua/luci/version.lua\nsed -i "s/luciversion = \\".*\\"/luciversion = \\"v'$(date +%Y%m%d)'\\"/g" /usr/lib/lua/luci/version.lua\necho "export const revision = '\''v'$(date +%Y%m%d)'\'\'', branch = '\''LuCI Master'\'';" > /usr/share/ucode/luci/version.uc\n/etc/init.d/rpcd restart\n' package/default-settings/default/zzz-default-settings
+
+# Shortcut Forwarding Engine
+git clone https://$gitea/sbwml/shortcut-fe package/new/shortcut-fe
+
+# firewall4 - master
+grm -rf package/network/config/firewall4
+gcp -a ../master/openwrt/package/network/config/firewall4 package/network/config/firewall4
+gsed -i 's|$(PROJECT_GIT)/project|https://github.com/openwrt|g' package/network/config/firewall4/Makefile
+mkdir -p package/network/config/firewall4/patches
+# fix ct status dnat
+curl -s https://$mirror/patch/firewall4/firewall4_patches/990-unconditionally-allow-ct-status-dnat.patch > package/network/config/firewall4/patches/990-unconditionally-allow-ct-status-dnat.patch
+# fullcone
+curl -s https://$mirror/patch/firewall4/firewall4_patches/999-01-firewall4-add-fullcone-support.patch > package/network/config/firewall4/patches/999-01-firewall4-add-fullcone-support.patch
+# bcm fullcone
+curl -s https://$mirror/patch/firewall4/firewall4_patches/999-02-firewall4-add-bcm-fullconenat-support.patch > package/network/config/firewall4/patches/999-02-firewall4-add-bcm-fullconenat-support.patch
+# kernel version
+curl -s https://$mirror/patch/firewall4/firewall4_patches/002-fix-fw4.uc-adept-kernel-version-type-of-x.x.patch > package/network/config/firewall4/patches/002-fix-fw4.uc-adept-kernel-version-type-of-x.x.patch
+# fix flow offload
+curl -s https://$mirror/patch/firewall4/firewall4_patches/001-fix-fw4-flow-offload.patch > package/network/config/firewall4/patches/001-fix-fw4-flow-offload.patch
+# add custom nft command support
+curl -s https://$mirror/patch/firewall4/100-openwrt-firewall4-add-custom-nft-command-support.patch | patch -p1
+# libnftnl
+grm -rf package/libs/libnftnl
+gcp -a ../master/openwrt/package/libs/libnftnl package/libs/libnftnl
+mkdir -p package/libs/libnftnl/patches
+curl -s https://$mirror/patch/firewall4/libnftnl/001-libnftnl-add-fullcone-expression-support.patch > package/libs/libnftnl/patches/001-libnftnl-add-fullcone-expression-support.patch
+curl -s https://$mirror/patch/firewall4/libnftnl/002-libnftnl-add-brcm-fullcone-support.patch > package/libs/libnftnl/patches/002-libnftnl-add-brcm-fullcone-support.patch
+sed -i '/PKG_INSTALL:=1/iPKG_FIXUP:=autoreconf' package/libs/libnftnl/Makefile
+# nftables
+grm -rf package/network/utils/nftables
+gcp -a ../master/openwrt/package/network/utils/nftables package/network/utils/nftables
+mkdir -p package/network/utils/nftables/patches
+curl -s https://$mirror/patch/firewall4/nftables/002-nftables-add-fullcone-expression-support.patch > package/network/utils/nftables/patches/002-nftables-add-fullcone-expression-support.patch
+curl -s https://$mirror/patch/firewall4/nftables/003-nftables-add-brcm-fullconenat-support.patch > package/network/utils/nftables/patches/003-nftables-add-brcm-fullconenat-support.patch
+
+# hide nftables warning message
+curl -s https://$mirror/patch/luci/luci-nftables.patch | patch -p1
+
+# FullCone module
+git clone https://$gitea/sbwml/nft-fullcone package/new/nft-fullcone
+
+# IPv6 NAT
+git clone https://$github/sbwml/packages_new_nat6 package/new/nat6
+
+# natflow
+git clone https://$github/sbwml/package_new_natflow package/new/natflow
+
+# Patch Luci add nft_fullcone/bcm_fullcone & shortcut-fe & natflow & ipv6-nat & custom nft command option
+pushd feeds/luci
+    curl -s https://$mirror/patch/firewall4/0001-luci-app-firewall-add-nft-fullcone-and-bcm-fullcone-.patch | patch -p1
+    curl -s https://$mirror/patch/firewall4/0002-luci-app-firewall-add-shortcut-fe-option.patch | patch -p1
+    curl -s https://$mirror/patch/firewall4/0003-luci-app-firewall-add-ipv6-nat-option.patch | patch -p1
+    curl -s https://$mirror/patch/firewall4/0004-luci-add-firewall-add-custom-nft-rule-support.patch | patch -p1
+    curl -s https://$mirror/patch/firewall4/0005-luci-app-firewall-add-natflow-offload-support.patch | patch -p1
+popd
