04ae6af62efddd296d57c5b2bef9a5727fa6a1b1
0118Add <60157610+0118Add@users.noreply.github.com>
2025-05-20 17:37:12 +0800

    Update diy-part2.sh

--- custom/diy-part2.sh
+++ custom/diy-part2.sh
@@ -172,8 +172,11 @@ sed -i 's/"admin/"admin\/services/g' feeds/luci/applications/luci-app-dockerman/
 
 # turboacc
 #git clone https://github.com/chenmozhijin/turboacc package/turboacc
-curl -sSL https://raw.githubusercontent.com/0118Add/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
-sed -i 's/Turbo ACC 网络加速/网络加速/g' package/turboacc/luci-app-turboacc/po/zh-cn/turboacc.po
+#curl -sSL https://raw.githubusercontent.com/0118Add/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
+#sed -i 's/Turbo ACC 网络加速/网络加速/g' package/turboacc/luci-app-turboacc/po/zh-cn/turboacc.po
+git clone https://github.com/0118Add/turboacc package/luci-app-turboacc
+git clone https://github.com/fullcone-nat-nftables/nft-fullcone package/nft-fullcone
+git clone -b package --single-branch https://github.com/0118Add/turboacc package/turboacc
 
 # net.netfilter.nf_conntrack_max from 16384 to 65535
 sed -i 's#net.netfilter.nf_conntrack_max=16384#net.netfilter.nf_conntrack_max=65535#g' package/kernel/linux/files/sysctl-nf-conntrack.conf
