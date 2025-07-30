8929850e8ceec6e8f12a541d5ff3b090a0db111e
0118Add <60157610+0118Add@users.noreply.github.com>
2025-07-30 10:38:20 +0800

    Update 6.16.sh

--- custom/6.16.sh
+++ custom/6.16.sh
@@ -69,7 +69,7 @@ sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' packag
 wget -O ./package/base-files/files/etc/banner https://raw.githubusercontent.com/0118Add/X86_64-Test/main/general/banner
 
 # Default settings
-git clone https://github.com/sbwml/default-settings package/default-settings
+git clone https://github.com/8688Add/default-settings package/default-settings
 
 # autoCore
 #merge_package https://github.com/0118Add/OP-Packages OP-Packages/autocore-arm
