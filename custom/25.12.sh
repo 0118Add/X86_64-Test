abd9facb636c5180360386f46b6818cfd172e811
0118Add <60157610+0118Add@users.noreply.github.com>
2026-03-09 12:36:55 +0800

    Update 25.12.sh

--- custom/25.12.sh
+++ custom/25.12.sh
@@ -162,8 +162,8 @@ sed -i '16s/ \\$//' feeds/luci/collections/luci/Makefile
 
 # ADD PKG 部分
 rm -rf feeds/packages/utils/coremark
-merge_package https://github.com/0118Add/OP-Packages OP-Packages/autocore-arm
-sed -i 's/+@KERNEL_DEBUG_INFO_BTF/+vmlinux-btf/' package/openwrt-einat-ebpf/Makefile
+merge_package https://github.com/QiuSimons/OpenWrt-Add OpenWrt-Add/openwrt-einat-ebpf
+sed -i 's/+@KERNEL_DEBUG_INFO_BTF/+vmlinux-btf/' custom/openwrt-einat-ebpf/Makefile
 git clone https://github.com/QiuSimons/vmlinux-btf package/vmlinux-btf
 
 # 替换文件
