feae79b0c4291e28ef5bdc6f09ae19518272fbce
0118Add <60157610+0118Add@users.noreply.github.com>
2026-06-21 15:49:29 +0800

    Update 25.12.sh

--- custom/25.12.sh
+++ custom/25.12.sh
@@ -170,6 +170,9 @@ sed -i '/PKG_INSTALL:=/iPKG_FIXUP:=autoreconf' package/libs/libnftnl/Makefile
 curl -sSL https://raw.githubusercontent.com/mufeng05/turboacc/main/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
 sed -i 's/Turbo ACC 网络加速/网络加速/g' package/turboacc/luci-app-turboacc/po/zh_Hans/turboacc.po
 
+# Realtek Ethernet driver - R8168 & R8125 & R8126 & R8152 & R8101 & r8127
+git_sparse_clone master https://github.com/coolsnowwolf/lede/package/kernel r8152
+
 # 移除luci-app-attendedsysupgrade
 sed -i '18d' feeds/luci/collections/luci-nginx/Makefile
 sed -i '17d' feeds/luci/collections/luci/Makefile
