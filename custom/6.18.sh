43b60652817396ba33264b162df249dd6c6df9fb
0118Add <60157610+0118Add@users.noreply.github.com>
2026-06-26 14:36:00 +0800

    Update 6.18.sh

--- custom/6.18.sh
+++ custom/6.18.sh
@@ -236,5 +236,23 @@ wget https://gist.githubusercontent.com/mj22226/363cefecd314e45b49d8eafff8473fcf
  wget https://gist.githubusercontent.com/mj22226/9a69bb754a83e8a5cf03fcc60d62d473/raw/cc6f36831d6098de4a7fccf04a312d36a0dbc999/01-key.patch
  git apply 01-key.patch
 
+# 拉取指定软件包
+function merge_package(){
+    trap 'rm -rf "$tmpdir"' EXIT
+    branch="$1" curl="$2" && shift 2
+    rootdir="$PWD"
+    localdir=package/apps
+    [ -d "$localdir" ] || mkdir -p "$localdir"
+    tmpdir="$(mktemp -d)" || exit 1
+    git clone -b "$branch" --depth 1 --filter=blob:none --sparse "$curl" "$tmpdir"
+    cd "$tmpdir"
+    git sparse-checkout init --cone
+    git sparse-checkout set "$@"
+    mv -f "$@" "$rootdir"/"$localdir" && cd "$rootdir"
+}
+merge_package main https://github.com/try496/openwrt-packages luci-app-cpufreq cpufreq luci-app-msd_lite msd_lite luci-app-ddns-go ddns-go
+
+merge_package main https://github.com/kenzok8/small-package luci-app-daed daed
+
 ./scripts/feeds update -i
 ./scripts/feeds install -a
