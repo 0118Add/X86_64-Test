1c5a786e47769314cc8c7fa81ba834ff749fbd38
0118Add <60157610+0118Add@users.noreply.github.com>
2026-07-16 15:16:16 +0800

    Update diy-part02.sh

--- custom/diy-part02.sh
+++ custom/diy-part02.sh
@@ -62,15 +62,14 @@ rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang
 
 # 预编译 node
-rm -rf feeds/packages/lang/node
-git clone --depth=1 -b packages-25.12 https://github.com/sbwml/feeds_packages_lang_node-prebuilt feeds/packages/lang/node
+#rm -rf feeds/packages/lang/node
+#git clone --depth=1 -b packages-25.12 https://github.com/sbwml/feeds_packages_lang_node-prebuilt feeds/packages/lang/node
 
 # 文件替换
 #wget -O ./package/base-files/files/etc/banner https://raw.githubusercontent.com/0118Add/X86_64-Test/main/general/banner
 #wget -O ./package/libs/libnftnl/Makefile https://raw.githubusercontent.com/0118Add/X86_64-Test/main/patch/Makefile
 
 # autoCore
-#git clone https://github.com/8688Add/autocore-arm -b openwrt-25.12 package/autocore
 git clone https://github.com/sbwml/autocore-arm -b openwrt-25.12 package/autocore
 
 # Default settings
