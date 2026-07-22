ed6af6bf4eb5855aa6a7fdb007040993c4ee4698
0118Add <60157610+0118Add@users.noreply.github.com>
2026-07-22 17:34:05 +0800

    Update lienol-24.10.sh

--- custom/lienol-24.10.sh
+++ custom/lienol-24.10.sh
@@ -156,10 +156,11 @@ git clone https://github.com/sirpdboy/luci-app-partexp package/luci-app-partexp
 
 # homeproxy
 #git clone --depth 1 -b dev https://github.com/immortalwrt/homeproxy package/luci-app-homeproxy
-#git clone --depth 1 -b main https://github.com/weicaixian86/luci-app-homeproxy package/luci-app-homeproxy
-git_sparse_clone main https://github.com/VIKINGYFY/packages luci-app-homeproxy
+git clone --depth 1 -b main https://github.com/weicaixian86/luci-app-homeproxy package/luci-app-homeproxy
+#git_sparse_clone main https://github.com/VIKINGYFY/packages luci-app-homeproxy
 sed -i "s/ImmortalWrt/OpenWrt/g" package/luci-app-homeproxy/po/zh_Hans/homeproxy.po
 sed -i "s/ImmortalWrt proxy/OpenWrt proxy/g" package/luci-app-homeproxy/htdocs/luci-static/resources/view/homeproxy/{client.js,server.js}
+rm -rf package/luci-app-homeproxy/.github/openwrt-packages/sing-box
 
 # daed
 git_sparse_clone kix https://github.com/QiuSimons/luci-app-daed daed luci-app-daed
