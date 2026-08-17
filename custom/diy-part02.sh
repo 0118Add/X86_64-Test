912274739fdbc4a73644dfe4a63b523656eff1fe
0118Add <60157610+0118Add@users.noreply.github.com>
2026-08-17 19:32:33 +0800

    Update diy-part02.sh

--- custom/diy-part02.sh
+++ custom/diy-part02.sh
@@ -176,8 +176,8 @@ git clone https://github.com/sbwml/luci-app-dockerman feeds/luci/applications/lu
 #git clone https://github.com/sbwml/packages_utils_containerd feeds/packages/utils/containerd
 #git clone https://github.com/sbwml/packages_utils_runc feeds/packages/utils/runc
 sed -i 's/"admin/"admin\/services/g' feeds/luci/applications/luci-app-dockerman/root/usr/share/luci/menu.d/luci-app-dockerman.json
-sed -i 's/PKG_RELEASE:=.*/PKG_RELEASE:=2/g' packages/utils/dockerd/Makefile
-curl -s https://raw.githubusercontent.com/0118Add/X86_64-Test/main/general/patches/001-skip-copy-nested-binaries.patch > packages/utils/dockerd/patches/001-skip-copy-nested-binaries.patch
+sed -i 's/PKG_RELEASE:=.*/PKG_RELEASE:=2/g' feeds/packages/utils/dockerd/Makefile
+curl -s https://raw.githubusercontent.com/0118Add/X86_64-Test/main/general/patches/001-skip-copy-nested-binaries.patch > feeds/packages/utils/dockerd/patches/001-skip-copy-nested-binaries.patch
 #sed -i 's/"admin",/"admin","services",/g' package/dockerman/applications/luci-app-dockerman/luasrc/controller/*.lua
 #sed -i 's/"admin/"admin\/services/g' package/dockerman/applications/luci-app-dockerman/luasrc/model/*.lua
 #sed -i 's/"admin/"admin\/services/g' package/dockerman/applications/luci-app-dockerman/luasrc/model/cbi/dockerman/*.lua
