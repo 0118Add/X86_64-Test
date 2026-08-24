94378389265b5148cd584df5a2a81c891b9274b4
0118Add <60157610+0118Add@users.noreply.github.com>
2026-08-24 20:21:52 +0800

    Fix repository URL in merge_package command

--- custom/Test.sh
+++ custom/Test.sh
@@ -178,7 +178,7 @@ rm -rf feeds/packages/utils/{docker,dockerd,containerd,runc}
 #git clone https://github.com/sbwml/packages_utils_dockerd feeds/packages/utils/dockerd
 #git clone https://github.com/sbwml/packages_utils_containerd feeds/packages/utils/containerd
 #git clone https://github.com/sbwml/packages_utils_runc feeds/packages/utils/runc
-merge_package master https://github.com/coolsnowwolf/packages feeds/packages/utils docker dockerd containerd runc
+merge_package master https://github.com/coolsnowwolf/packages/utils feeds/packages/utils docker dockerd containerd runc
 sed -i 's/"admin/"admin\/services/g' feeds/luci/applications/luci-app-dockerman/root/usr/share/luci/menu.d/luci-app-dockerman.json
 #mkdir -p feeds/packages/utils/dockerd/patches
 #curl -s https://raw.githubusercontent.com/0118Add/X86_64-Test/main/general/patches/001-skip-copy-nested-binaries.patch > feeds/packages/utils/dockerd/patches/001-skip-copy-nested-binaries.patch
