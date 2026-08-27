3bd904e5720c0ef52eb4b66ddf6c342b22f65d0d
0118Add <60157610+0118Add@users.noreply.github.com>
2026-08-27 17:56:48 +0800

    Clone kenzok8 openwrt-clashoo repository
    Added cloning of the kenzok8 openwrt-clashoo repository.

--- SCRIPTS/02_prepare_package.sh
+++ SCRIPTS/02_prepare_package.sh
@@ -259,6 +259,8 @@ sed -i "s/ImmortalWrt/OpenWrt/g" package/luci-app-homeproxy/po/zh_Hans/homeproxy
 sed -i "s/ImmortalWrt proxy/OpenWrt proxy/g" package/luci-app-homeproxy/htdocs/luci-static/resources/view/homeproxy/{client.js,server.js}
 git clone https://github.com/Openwrt-Passwall/openwrt-passwall-packages package/openwrt-passwall
 
+git clone https://github.com/kenzok8/openwrt-clashoo package/clashoo
+
 git clone --depth=1 -b openwrt-25.12 https://github.com/immortalwrt/luci.git immortalwrt-luci
 cp -rf immortalwrt-luci/applications/luci-app-diskman feeds/luci/applications/luci-app-diskman
 ln -sf ../../../feeds/luci/applications/luci-app-diskman ./package/feeds/luci/luci-app-diskman
