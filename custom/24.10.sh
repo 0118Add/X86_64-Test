7d9c320b13def2faf8fd5146503719ed648fcdd5
0118Add <60157610+0118Add@users.noreply.github.com>
2026-09-11 12:59:26 +0800

    Update default settings and golang package sources

--- custom/24.10.sh
+++ custom/24.10.sh
@@ -72,7 +72,8 @@ git clone https://github.com/sbwml/autocore-arm -b openwrt-24.10 package/autocor
 #curl -fsSL https://raw.githubusercontent.com/0118Add/X86_64-Test/main/10_system.js > package/autocore/files/generic/10_system.js
 
 # Default settings
-merge_package https://github.com/8688Add/openwrt_pkgs openwrt_pkgs/default-settings
+git clone https://github.com/sbwml/default-settings -b openwrt-24.10 package/default-settings
+#merge_package https://github.com/8688Add/openwrt_pkgs openwrt_pkgs/default-settings
 
 # golang 26.x
 rm -rf feeds/packages/lang/golang
